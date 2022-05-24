4798b1f388d49cff91199db4adfadd40626fe6e3
Zhichao Zhang <zzcwoshizz@163.com>
2022-05-24 10:47:00 +0800

    tutorial bg

--- packages/app/src/TutorialFrame/index.tsx
+++ packages/app/src/TutorialFrame/index.tsx
@@ -1,22 +1,29 @@
 import styled from '@emotion/styled';
 import { Box } from '@mui/material';
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useMatch, useResolvedPath } from 'react-router-dom';
 
 import Header from './Header';
 
-const Wrapper = styled(Box)`
-  width: 100vw;
-  min-height: 100vh;
-  padding-bottom: 44px;
-  background: url('/images/tutorial-bg.webp') no-repeat;
-  background-size: cover;
-  background-position: center;
-`;
+const Wrapper = styled(Box)<{ inTutorial: boolean }>(
+  ({ inTutorial }) => `
+width: 100vw;
+min-height: 100vh;
+padding-bottom: 44px;
+background: url('${
+    inTutorial ? '/images/tutorial-bg2.webp' : '/images/tutorial-bg.webp'
+  }') no-repeat;
+background-size: cover;
+background-position: center;
+`
+);
 
 const TurorialFrame: React.FC = () => {
+  const tutorialResolved = useResolvedPath('/tutorial');
+  const match = useMatch({ path: tutorialResolved.pathname, end: true });
+
   return (
-    <Wrapper>
+    <Wrapper inTutorial={!!match}>
       <Header />
       <Outlet />
     </Wrapper>
