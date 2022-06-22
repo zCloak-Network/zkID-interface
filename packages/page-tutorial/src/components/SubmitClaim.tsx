c6c467827b40e0e7b6bf152087e8a70349e78e5a
Zhichao Zhang <zzcwoshizz@163.com>
2022-06-22 12:09:54 +0800

    change step text

--- packages/page-tutorial/src/components/SubmitClaim.tsx
+++ packages/page-tutorial/src/components/SubmitClaim.tsx
@@ -157,7 +157,11 @@ const SubmitClaim: React.FC<Props> = ({ contents, reportError }) => {
     <>
       <Recaptcha onCallback={setToken} />
       <StayAlert
-        message={`We are checking your documents. The attestation in queued ${position}`}
+        message={
+          position
+            ? `Your request is No.${position} in the attestation queue...`
+            : 'Request sent. We are checking your document...'
+        }
         open={loading || attestationStatus === AttestationStatusV2.submiting}
         severity="warning"
       />
